1defb73d85ed42ac138d468b1061af9a80f1a00d
dandyraka <dandyraka13@gmail.com>
2020-12-12 16:16:59 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const {normalize, to_vlang} = require('./lib/vlang');
+
+let teks = "bengek hyung";
+console.log(to_vlang(teks));
+console.log(normalize(teks));
---
+++ readme.md
@@ -0,0 +1,5 @@
+# V-Lang
+
+Gatau ini bahasa apa, populer banget ini pas aku SD.
+
+Basicnya sih nambahin **`va/vi/vu/ve/vo`** aja sih di setiap huruf vokal
